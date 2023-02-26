f1613ea05d2207f8a3ceaf7ea1bf88b39fbf544c
Michael Davies <mike@netly.co.uk>
2023-02-26 13:39:22 +0000

    created class declarations for Engineer, Intern, and Manager

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const Manager = require("./lib/Manager");
+
+const Employee = require("./lib/Employee");const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
@@ -10,6 +11,17 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+let Mike = new Employee
+let Steve = new Engineer
+let Jo = new Intern
+let Mark = new Manager
+
+console.log(Mike.getRole)
+console.log(Steve.getRole)
+console.log(Jo.getRole)
+console.log(Mark.getRole)
+
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+// Use inquirer to ask questions
 
