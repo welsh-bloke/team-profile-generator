e25d63d3c15f3790281e2212eeeffab8d46e215d
Michael Davies <mike@netly.co.uk>
2023-02-28 18:09:50 +0000

    Finished adding content to thr README.md file and made some adjustments to page-template.js

---
+++ README.md
@@ -0,0 +1,58 @@
+# Team Profile Generator
+
+![GitHub](https://img.shields.io/github/license/welsh-bloke/prework-study-guide?logo=GitHub&style=flat-square)
+
+## Overview
+
+The Team Profile Generator web app was created using Inquirer.js, for the purpose of generating a 'My Team' web page. It generates the page by using the terminal 
+to ask series of questions. When complete, thr program generates a 'team.html page. The app asks the following questions:
+
+* Manager Questions
+    * What's your manager's name?
+    * What's your manager's id?
+    * What's your manager's email?
+    * What's your manager's office number?
+* Engineer Questions?
+    * What's your engineer's name?
+    * What's your engineer's id?
+    * What's your engineer's email?
+    * What's your engineer's Github account url?
+* Intern Questions?
+    * What's your intern's name?
+    * What's your intern's name?
+    * What's your intern's name?
+    * What School did this intern attend?
+
+After each completed secion, the user is prompted to choose one of the following actions:
+
+* What would you like to do next?
+    * Add a manager
+    * Add an engineer
+    * Add an intern
+    * Finish building the team
+
+## Validation
+
+If the user tries to input an empty value, they are presented with a warning, and the question is repeated:
+
+## Screenshots
+
+### Terminal Questions And Answers
+
+<img src="./src/assets/terminal.png" width="600">
+
+### HTML Output
+
+<img src="./src/assets/output.png" width="600">
+
+## Deployment
+
+This app was not deployed
+
+## Usage
+
+Open the app in a terminal and run 'node index.js
+
+## License
+
+This project is licensed under the MIT license.
--- src/page-template.js
+++ src/page-template.js
@@ -4,57 +4,63 @@ const generateTeam = team => {
     // creates the manager html
     const generateManager = manager => {
         return `
-        <div class="card employee-card">
-        <div class="card-header">
-            <h2 class="card-title">${manager.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
-        </div>
-        <div class="card-body">
-            <ul class="list-group">
-                <li class="list-group-item">ID: ${manager.getId()}</li>
-                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
-            </ul>
-        </div>
-    </div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        <h2 class="card-title">${manager.getName()}</h2>
+                        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item">ID: ${manager.getId()}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                            <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         `;
     };
 
     // creates the html for engineers
     const generateEngineer = engineer => {
         return `
-        <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${engineer.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
-        </ul>
-    </div>
-</div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        <h2 class="card-title">${engineer.getName()}</h2>
+                        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item">ID: ${engineer.getId()}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         `;
     };
 
     // creates the html for interns
     const generateIntern = intern => {
         return `
-        <div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">${intern.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${intern.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-            <li class="list-group-item">School: ${intern.getSchool()}</li>
-        </ul>
-    </div>
-</div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        <h2 class="card-title">${intern.getName()}</h2>
+                        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item">ID: ${intern.getId()}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                            <li class="list-group-item">School: ${intern.getSchool()}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         `;
     };
 
@@ -91,8 +97,8 @@ module.exports = team => {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>My Team</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
     <link rel="stylesheet" href="style.css">
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
 </head>
@@ -100,16 +106,14 @@ module.exports = team => {
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
+            <div class="col team-heading">
                 <h1 class="text-center">My Team</h1>
             </div>
         </div>
     </div>
     <div class="container">
-        <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center">
-                ${generateTeam(team)}
-            </div>
+        <div class="row row-cols-1 row-cols-md-3 g-4">            
+            ${generateTeam(team)}            
         </div>
     </div>
 </body>
